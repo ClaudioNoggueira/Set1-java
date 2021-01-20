13a27c8748e08dcb401434abc04dc0e0aac3e2a7
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-20 18:02:41 -0300

    Implementação inicial

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,40 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
+
 public class Program {
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Entre com o caminho do diretório: ");
+		String path = input.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			Set<LogEntry> set = new HashSet<>();
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] vect = line.split(" ");
+				set.add(new LogEntry(vect[0], Date.from(Instant.parse(vect[1]))));
+				line = br.readLine();
+			}
+			
+			System.out.println("Total de usuários: " + set.size());
+		} catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} finally {
+			if (input != null)
+				input.close();
+		}
 	}
 }
